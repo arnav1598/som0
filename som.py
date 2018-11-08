2a6e4afb1f9ef937628b726219a607b6c435d3a5
Arnav Sachdeva <34678814+arnav1598@users.noreply.github.com>
2018-11-09 03:27:01 +0530

    Update som.py

--- som.py
+++ som.py
@@ -34,18 +34,35 @@ X=X[:, [0, 2, 4, 6, 8, 10, 12, 14, 15, 17, 18, 19, 20, 21, 22, 23]]
 
 # Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (-1, 1))
+sc = MinMaxScaler(feature_range = (0, 1))
 X = sc.fit_transform(X)
 
 # Training the SOM
-map_dim=8
+map_dimx=15
+map_dimy=10
 from minisom import MiniSom
-som = MiniSom(x = map_dim, y = map_dim, input_len = 16, sigma = 1.0, learning_rate = 0.5)
+som = MiniSom(x = map_dimx, y = map_dimy, input_len = 16, sigma = 1.0, learning_rate = 0.5)
 som.random_weights_init(X)
-som.train_random(data = X, num_iteration = 50)
+som.train_random(data = X, num_iteration = 100)
 
 #Creating the map
 from collections import defaultdict
 mappings = defaultdict(list)
 for i, x in enumerate(X):
             mappings[som.winner(x)].append(y[i])
+
+#Visualisation of the mapping
+fact=2.
+plt.figure(figsize=(map_dimx*fact, map_dimy*fact))
+for i, x in enumerate(X):
+    winnin_position = som.winner(x)
+    plt.text(x=winnin_position[0]*fact+fact/2, 
+             y=winnin_position[1]*fact+np.random.rand()*(fact-0.1),
+             horizontalalignment='center',
+             fontsize='large',
+             s=y[i],
+             color=(winnin_position[0]/map_dimx, 1-(winnin_position[0]/map_dimx), winnin_position[1]/map_dimy, 1))
+plt.xlim([0, map_dimx*fact])
+plt.ylim([0, map_dimy*fact])
+plt.axis('off')
+plt.plot()
